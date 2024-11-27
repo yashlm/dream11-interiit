24e6a33af8505d5a5bc34a13d0dbf42740df3224
Akshat <iitkakshat@gmail.com>
2024-11-28 01:02:18 +0530

    refactor Home component layout for improved structure and readability

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,24 +1,21 @@
-import Header from "../header/Header";
- import ListsOfMovies from "../movieslist/ListsOfMovies";
-import Calendar from "../movieslist/Calendar";
+
 // import { Grid } from "../howtoplay/grid";
+import ListsOfMovies from "../movieslist/ListsOfMovies";
+import Calendar from "../movieslist/Calendar";
+import Header from "../header/Header";
 
 const Home = () => {
   return (
 
     <div>
       <Header />
-      <div style={{ display: "flex", width: "100vw" }} className="bg-green-700">
-        <div>
-          <ListsOfMovies /> 
-        </div>
-        <div >
-          <Calendar/>
-          {/* <Grid/> */}
-        </div>
-
+      <div style={{ display: "flex", width: "100vw" }}>
+      <ListsOfMovies /> 
+       <div style={{ flex: "0 0 30%", padding: "20px" }}>
+       <Calendar/>
+       {/* <Grid/> */}
      </div>
-      
+     </div>
     </div>
   );
 };
