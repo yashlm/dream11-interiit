6bff21ac4e6f63fbbdec7919ab7802832ca0a209
Yash Chauhan <yashc22@iitk.ac.in>
2024-11-28 10:29:24 +0530

    aboutus and faqs complete

--- frontend/src/Starterpage/Starterpage.jsx
+++ frontend/src/Starterpage/Starterpage.jsx
@@ -5,14 +5,16 @@ import './StarterPage.css';
 import Navbar from '../header/Navbar';
 import Testinomial from '../component/testinomials';
 import { FooterWithSocialLinks } from '../component/footer';
-import logo from '../assets/landing_page/dream11logo.svg'
+import logo from '../assets/landing_page/dream11logo.svg';
+import FAQ from '../component/faqs';
+import AboutUs from '../component/AboutUs';
 
 const App = () => {
   const [username, setUsername] = useState('');
   const navigate = useNavigate();
 
   const handleNextClick = () => {
-    navigate("/home");
+    navigate('/home');
   };
 
   return (
@@ -21,7 +23,8 @@ const App = () => {
       <div className="main-container">
         <div className="content-container">
           <h1 className="welcome-text">
-            Welcome to <span className='dream11'><img src={logo}></img></span>  <span className='lets'>Let’s Get Started</span>
+            Welcome to <span className="dream11"><img src={logo} alt="Dream11 Logo" /></span>
+            <span className="lets">Let’s Get Started</span>
           </h1>
           <div className="form-container">
             <TextField
@@ -31,25 +34,21 @@ const App = () => {
               variant="outlined"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
-              inputProps={{ style: { color: "white", } }}
-              InputLabelProps={{ style: { color: "#808080" } }}
+              InputProps={{ style: { color: 'white' } }}
+              InputLabelProps={{ style: { color: '#808080' } }}
               sx={{
-                "& .MuiOutlinedInput-notchedOutline": {
-                  border: "2px solid #fcf3f2"
+                '& .MuiOutlinedInput-notchedOutline': {
+                  border: '2px solid #fcf3f2',
                 },
-
-                "& .MuiOutlinedInput-root": {
-                  "&.Mui-focused fieldset": {
-                    border: "2px solid #e6e6e6"
+                '& .MuiOutlinedInput-root': {
+                  '&.Mui-focused fieldset': {
+                    border: '2px solid #e6e6e6',
                   },
-                  "&:hover:not(.Mui-focused)": {
-                    "& .MuiOutlinedInput-notchedOutline": {
-                      borderColor: "#fcf3f2",
-                    },
+                  '&:hover:not(.Mui-focused) .MuiOutlinedInput-notchedOutline': {
+                    borderColor: '#fcf3f2',
                   },
-                }
+                },
               }}
-
             />
             <button className="next-btn" onClick={handleNextClick}>
               Next
@@ -57,7 +56,9 @@ const App = () => {
           </div>
         </div>
       </div>
+      <AboutUs />
       <Testinomial />
+      <FAQ />
       <FooterWithSocialLinks />
     </div>
   );
