95ee25065bd81ba24111b30b13d89c35e692136d
Yash Chauhan <yashc22@iitk.ac.in>
2024-12-01 08:29:58 +0530

    fixed build issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import "./css/App.css";
 import "./css/index.css";
 import "./css/output.css";
-import { Component, useState } from "react";
+import { useState } from "react";
 import "./css/style.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import StarterPage from "./pages/Starterpage";
@@ -14,30 +14,31 @@ import Loading from "./component/Loading";
 import CustomMatch from "./pages/CustomMatch";
 import CustomMatchcsv from "./pages/CustomMatchcsv";
 // import DescriptionCard from "./component/matchDescriptionCard";
-import MatchDetails from "./pages/MatchDetails";
+// import MatchDetails from "./pages/MatchDetails";
 import HomePage from "./pages/HomePage";
 
 
 function App() {
   const [isChatOpen, setIsChatOpen] = useState(false);
-    return (
-      <div className="App">
-        <div className="appbot">
-          <ChatBot isChatOpen={isChatOpen} setIsChatOpen={setIsChatOpen} />
-          <FloatingButton setIsChatOpen={setIsChatOpen} isChatOpen={isChatOpen} />
-        </div>
-        <Routes>
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/custommatch/:date" element={<CustomMatch />} />
-          <Route path="/custommatch" element={<CustomMatchcsv />} />
-          <Route path="/teamSelect" element={<CardStack />} />
-          <Route path="/loading" element={<Loading />} />
-          <Route path="/dreamTeam/:match_id" element={<DreamTeamGround />} />
-          <Route path="/" element={<StarterPage />} />
-          <Route path="/fav" element={<FavTeam />} />
-        </Routes>
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <div className="appbot">
+        <ChatBot isChatOpen={isChatOpen} setIsChatOpen={setIsChatOpen} />
+        <FloatingButton setIsChatOpen={setIsChatOpen} isChatOpen={isChatOpen} />
       </div>
-    </BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/custommatch/:date" element={<CustomMatch />} />
+        <Route path="/custommatch" element={<CustomMatchcsv />} />
+        <Route path="/teamSelect" element={<CardStack />} />
+        <Route path="/loading" element={<Loading />} />
+        <Route path="/dreamTeam/:match_id" element={<DreamTeamGround />} />
+        <Route path="/" element={<StarterPage />} />
+        <Route path="/fav" element={<FavTeam />} />
+      </Routes>
+    </div>
+    </BrowserRouter >
   );
 }
 
